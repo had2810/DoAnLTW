4500015f7ef4f233009647560f798ad54cf95f38
ducdat0402 <leducdat0402@gmail.com>
2025-03-02 10:44:09 +0700

    Add đăng nhập và đăng kí

---
+++ DoAnLTW/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnLTW.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
