4e62d40231e2e0854e4c762e8ae73b47ca075218
had2810 <hoanganhduc42115@gmail.com>
2025-03-02 09:14:00 +0700

    first code

---
+++ DoAnLTW/Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnLTW.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
